417eacf51639edcfd0a83bc21e4459fa467ead4f
Erfan Ebrahimi <persionhost@gmail.com>
2022-11-23 00:51:26 +0330

    add tailwindcss with npm

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./package/yeganehha/laravel-dynamic-forms/Resources/**/*.blade.php",
+      "./package/yeganehha/laravel-dynamic-forms/Resources/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
